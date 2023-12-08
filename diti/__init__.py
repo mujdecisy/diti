cadd85226ee5b71744b56e0c5eaea2fa3c197c96
mujdecisy <mujdecisy@gmail.com>
2023-12-08 23:01:54 +0300

    fix: add missing exports

--- diti/__init__.py
+++ diti/__init__.py
@@ -1,5 +1,14 @@
 from diti.diti_obj import Diti
 from diti.diti_interval import DitiInterval as diti_interval
-from diti.diti_ops import diti_op, DitiOps
+from diti.diti_ops import (
+    diti_op,
+    DitiOpAdd,
+    DitiOpAlignTo,
+    DitiOpHeadOf,
+    DitiOpTailOf,
+    DitiOpTimezoneChange,
+    DitiOpTimezoneUpdate,
+    DitiOpUpdate,
+)
 from diti.constants import DitiParts, DitiRound
 from diti.timezones import DitiTimezone
