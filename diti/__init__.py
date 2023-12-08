98eb7904be2d51e6aab13ad50cea209fe1f5c6b1
mujdecisy <mujdecisy@gmail.com>
2023-12-09 00:31:27 +0300

    chore: update version & add export

--- diti/__init__.py
+++ diti/__init__.py
@@ -1,5 +1,6 @@
 from diti.diti_obj import Diti
 from diti.diti_interval import DitiInterval as diti_interval
+from diti.calcs import DitiCalcs as diti_calcs
 from diti.diti_ops import (
     diti_op,
     DitiOpAdd,
